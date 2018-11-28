c7bb3575e9bc2f0822e242392b51f7d06b239518
Joy <stepjoyous@gmail.com>
2018-11-28 12:56:41 -0800

    Completed task of on click add new item to top of chore list

--- 02_jquery_todo_list/starter-code/js/main.js
+++ 02_jquery_todo_list/starter-code/js/main.js
@@ -1,1 +1,17 @@
 // your code here:
+
+// OBJECTIVE: Make it work
+
+// Part A: Exercise 1
+    var $list = $('.list');
+    var $topList = $('#todo');
+        console.log($list);
+        console.log($topList);
+    var $button = $('.btn');
+
+    //On click, add #todo to top of list on button click
+    $button.on("click", function(evt){
+        evt.preventDefault();
+        $list.prepend($topList);
+        console.log('New chore added!');
+    });
